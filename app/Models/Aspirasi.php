dab146066eb5cf936cc7aa881076f50467d260dc
vauzi <ismanvauzi998@gmail.com>
2022-12-24 10:03:08 +0700

    (-) modify model (-) create new file AspirasiController

--- app/Models/Aspirasi.php
+++ app/Models/Aspirasi.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Aspirasi extends Model
 {
     use HasFactory;
+
+    protected $guarded = [
+        'id',
+        'created_at',
+        'updated_at',
+    ];
 }
