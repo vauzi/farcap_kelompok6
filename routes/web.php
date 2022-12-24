a8ad20263975e8c722c71f01d0dc8a0f18b1f029
vauzi <ismanvauzi998@gmail.com>
2022-12-24 11:15:34 +0700

    (-) modify Aspirasi Controller  (-)modify function create (-)modify function store

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AspirasiController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::prefix('aspirasi')->name('aspirasi.')->controller(AspirasiController::class)->group(function () {
+    Route::get('/', 'create')->name('create');
+    Route::post('/store', 'store')->name('store');
 });
